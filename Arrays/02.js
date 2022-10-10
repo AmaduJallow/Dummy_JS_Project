0c0f06c0bc1cb837ca728a3e636c4ecd91b5561f
Amadu Jallow <aj22011021@utg.edu.gm>
2022-10-10 15:14:44 +0000

    Minor updates

--- Arrays/02.js
+++ Arrays/02.js
@@ -1,5 +1,22 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7];
+const numbers = [1, 2, 3, 4, 5, 6, 7,7,7,7,9];
 
 let value = numbers.indexOf(3);
-
 console.log(value);  
+value = numbers.lastIndexOf(7);
+console.log(value);  
+value = numbers.includes(6);
+console.log(value);  
+
+//  !Reference Type
+
+let courses = [
+    {
+        id: 0,
+        course_name: "Javascript"
+    },
+    {
+        id: 1,
+        course_name: "Java"
+    }
+    
+]
