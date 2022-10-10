14fe35710622b8bbe2496bbe9a29b47a58fd7fae
Amadu Jallow <aj22011021@utg.edu.gm>
2022-10-10 11:08:53 +0000

    Creating object in javascript

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// !Object Literal Syntax
 const circle = {
     radius: 1,
     location: {
@@ -10,3 +11,33 @@ const circle = {
     }
 }
 circle.draw()
+
+// !Factorie function
+function createCircle(radius, x_value = 1, y_value = 1) {
+    return {
+        radius: radius,
+        x_value: x_value,
+        y_value: y_value,
+        draw: function () {
+            console.log(this.radius * x_value + this.radius * y_value);
+        }
+    }
+    
+}
+
+let cir = createCircle(2, 5, 7);
+cir.draw();
+
+// !Constructor Function for creating an object
+function Circle(radius, x_value = 1, y_value = 1) { 
+    this.radius = radius;
+    this.x_value = x_value;
+    this.y_value = y_value;
+    this.draw = function () {
+        console.log("drawn");
+    }
+
+};
+
+cir = new Circle(4);
+cir.draw();
